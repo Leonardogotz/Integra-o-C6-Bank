da1ba4a87d5ee7ddf6a442e743dfa2b0550e060e
Leonardogotz <141958239+Controlloil@users.noreply.github.com>
2025-02-06 14:30:27 -0300

    update

--- config/config.py
+++ config/config.py
@@ -8,10 +8,10 @@ CERTS_DIR = BASE_DIR / "certs"
 # Configurações para diferentes filiais
 FILIAIS = {
     "Toolkit": {
-        # CLIENTE_ID: 6d651c51-5c59-4c2c-8e56-19bc19a4b298
-        # CLIENTE_SECRET: vw67M2S92fsWVAVgAkGgJmm6e9uzvzGd
-        "client_id": os.getenv("C6_CLIENT_ID_TOOLKIT", "4804b8af-0f37-40f9-a8ae-a6f90da3d2f4"),
-        "client_secret": os.getenv("C6_CLIENT_SECRET_TOOLKIT", "LJL7MoFDnKtTJS2Xs5dSu2ku7HAPKtrN"),
+        # CLIENTE_ID: 01ea9d70-e868-4964-8f66-695f98a06298
+        # CLIENTE_SECRET: 1ZM68PYLrWX2ISzmvx572IGQdeU2nqKJ
+        "client_id": os.getenv("C6_CLIENT_ID_TOOLKIT", "5f448d96-f686-46f7-90cc-9d167a51778f"),
+        "client_secret": os.getenv("C6_CLIENT_SECRET_TOOLKIT", "X8wT2jZFeAGOTyMrw9oH4V1uLA20oVsz"),
         "cert": {
             "client_cert": str(CERTS_DIR / "certToolKit.crt"),
             "client_key": str(CERTS_DIR / "certToolKit.key")
@@ -19,10 +19,10 @@ FILIAIS = {
         "grant_type": "client_credentials"
     },
     "Inteligente TI": {
-        # CLIENTE_ID: 01ea9d70-e868-4964-8f66-695f98a06298
-        # CLIENTE_SECRET: 1ZM68PYLrWX2ISzmvx572IGQdeU2nqKJ
-        "client_id": os.getenv("C6_CLIENT_ID_INTELIGENTETI", "5f448d96-f686-46f7-90cc-9d167a51778f"),
-        "client_secret": os.getenv("C6_CLIENT_SECRET_INTELIGENTETI", "X8wT2jZFeAGOTyMrw9oH4V1uLA20oVsz"),
+        # CLIENTE_ID: 6d651c51-5c59-4c2c-8e56-19bc19a4b298
+        # CLIENTE_SECRET: vw67M2S92fsWVAVgAkGgJmm6e9uzvzGd
+        "client_id": os.getenv("C6_CLIENT_ID_INTELIGENTETI", "4804b8af-0f37-40f9-a8ae-a6f90da3d2f4"),
+        "client_secret": os.getenv("C6_CLIENT_SECRET_INTELIGENTETI", "LJL7MoFDnKtTJS2Xs5dSu2ku7HAPKtrN"),
         "cert": {
             "client_cert": str(CERTS_DIR / "certInteligente.crt"),
             "client_key": str(CERTS_DIR / "certInteligente.key")
